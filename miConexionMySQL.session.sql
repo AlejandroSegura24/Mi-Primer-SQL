00065f1f7f47dfb5de3f860cb70382f8d1e16c1f
Alejandro Segura <davidalejandrocmbs@gmail.com>
2025-05-09 10:24:53 -0500

    primeros cambios

---
+++ miConexionMySQL.session.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clients (
+    name varchar(100),
+    email varchar(60),
+    phone_number varchar(15)
+);
+
+CREATE TABLE products (
+    name VARCHAR(20),
+    sku VARCHAR(15),
+    slug VARCHAR(20),
+    description TEXT,
+    price FLOAT
+);
